e94e3ad332242cae875e032a7d4191494d6e6726
Scott Morken <smorken@nrcan.gc.ca>
2019-09-17 14:42:50 -0700

    class for indexing localized archive index databases

---
+++ cbm_defaults/archive_index.py
@@ -0,0 +1,14 @@
+
+
+class ArchiveIndex:
+
+    def __init__(self, locales, default_locale, archive_index_data):
+        self.locales = locales
+        self.default_locale = default_locale
+        self.archive_index_data = archive_index_data
+
+    def get_archive_index_database(locale=None):
+        pass
+
+    def query(sql, locale=None):
+        pass
--- cbm_defaults/populate_cbm_defaults.py
+++ cbm_defaults/populate_cbm_defaults.py
@@ -8,19 +8,24 @@ start_logging("populate_cbm_defaults.log")
 aidb_dir = r"M:\CBM Tools and Development\Builds\OpScaleArchiveIndex\20180828"
 
 locales = [
-    {"id": 1, "code": "en-CA" },
-    {"id": 2, "code": "fr-CA" },
-    {"id": 3, "code": "es-MX" },
-    {"id": 4, "code": "ru-RU" },
-    {"id": 5, "code": "pl-PL" }
+    {"id": 1, "code": "en-CA"},
+    {"id": 2, "code": "fr-CA"},
+    {"id": 3, "code": "es-MX"},
+    {"id": 4, "code": "ru-RU"},
+    {"id": 5, "code": "pl-PL"}
 ]
-aidb_paths = {
-    "en-CA": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install.mdb"),
-    "fr-CA": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_fr.mdb"),
-    "es-MX": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_es.mdb"),
-    "ru-RU": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_ru.mdb"),
-    "pl-PL": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install.mdb")#since there is no polish aidb, use the english terms
-}
+archive_index_data = [
+    {"locale": "en-CA",
+     "path": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install.mdb")},
+    {"locale": "fr-CA",
+     "path": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_fr.mdb")},
+    {"locale": "es-MX",
+     "path": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_es.mdb")},
+    {"locale": "ru-RU",
+     "path": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install_ru.mdb")},
+    {"locale": "pl-PL",
+     "path": os.path.join(aidb_dir, "ArchiveIndex_Beta_Install.mdb")} #since there is no polish aidb, use the english terms
+]
 
 cbmDefaultsPath = r"C:\dev\cbm_defaults\cbm_defaults.db"
 ddlPath = "cbmDefaults.ddl"
