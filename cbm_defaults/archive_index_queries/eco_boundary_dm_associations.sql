ec7d8dfe198c05279e0e411838d39157caa630f9
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-10-11 15:04:07 +0200

    Better readable query

--- cbm_defaults/archive_index_queries/eco_boundary_dm_associations.sql
+++ cbm_defaults/archive_index_queries/eco_boundary_dm_associations.sql
@@ -1,9 +1,15 @@
-SELECT tblDMAssociationDefault.DefaultDisturbanceTypeID,
-tblSPUDefault.SPUID, tblDMAssociationDefault.DMID
-FROM tblDMAssociationDefault INNER JOIN tblSPUDefault ON
-tblDMAssociationDefault.DefaultEcoBoundaryID =
-tblSPUDefault.EcoBoundaryID
-GROUP BY tblDMAssociationDefault.DefaultDisturbanceTypeID,
-tblSPUDefault.SPUID, tblDMAssociationDefault.DMID,
-tblDMAssociationDefault.DefaultDisturbanceTypeID
-HAVING tblDMAssociationDefault.DefaultDisturbanceTypeID<>1;
+SELECT
+    tblDMAssociationDefault.DefaultDisturbanceTypeID,
+    tblSPUDefault.SPUID,
+    tblDMAssociationDefault.DMID
+FROM
+    tblDMAssociationDefault
+INNER JOIN tblSPUDefault ON
+    tblDMAssociationDefault.DefaultEcoBoundaryID = tblSPUDefault.EcoBoundaryID
+GROUP BY
+    tblDMAssociationDefault.DefaultDisturbanceTypeID,
+    tblSPUDefault.SPUID,
+    tblDMAssociationDefault.DMID,
+    tblDMAssociationDefault.DefaultDisturbanceTypeID
+HAVING
+    tblDMAssociationDefault.DefaultDisturbanceTypeID<>1;
