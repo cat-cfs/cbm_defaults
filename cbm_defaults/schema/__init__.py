510c8b911936a0d42874f95bb090107183f54293
Scott <scottm361@gmail.com>
2021-10-12 16:46:28 -0700

    moving files to support packaging

---
+++ cbm_defaults/schema/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+
+def get_ddl_path():
+    """Gets the path to the ddl file bundled with this package
+    """
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(this_directory, "cbmDefaults.ddl")
