2045eca8b03d2ec5c95d137a030f5b5d796ade40
Scott <scottm361@gmail.com>
2021-10-13 12:47:32 -0700

    integration testing

---
+++ generate_coverage.bat
@@ -0,0 +1,2 @@
+pytest --cov=cbm_defaults --cov-report html:cov_html
+coverage-badge -o coverage.svg -f
--- test/integration/integration_test.py
+++ test/integration/integration_test.py
@@ -9,29 +9,27 @@ from cbm_defaults import schema
 class IntegrationTest(unittest.TestCase):
 
     def test_integration(self):
-        temp_dir = "."
+        with TemporaryDirectory() as temp_dir:
 
-        en_ca_aidb = os.path.join(temp_dir, "en_ca_aidb.mdb")
-        es_mx_aidb = os.path.join(temp_dir, "es_mx_aidb.mdb")
-        import_parameters.import_aidb_parameters(
-            aidb_path=en_ca_aidb, parameters_dir=None, mode="write",
-            locale="en-CA")
-        import_parameters.import_aidb_parameters(
-            aidb_path=es_mx_aidb, parameters_dir=None, mode="write",
-            locale="es-MX")
-        app.run(
-            {
-                "output_path": os.path.join(temp_dir, "cbm_defaults.db"),
-                "schema_path": schema.get_ddl_path(),
-                "default_locale": "en-CA",
-                "locales": [
-                    {"id": 1, "code": "en-CA"},
-                    {"id": 2, "code": "es-MX"}
-                ],
-                "archive_index_data": [
-                    {"locale": "en-CA",
-                     "path": en_ca_aidb},
-                    {"locale": "es-MX",
-                     "path": es_mx_aidb},
-                ]
-            })
+            locales = ["en-CA", "fr-CA", "es-MX", "ru-RU"]
+            for locale in locales:
+                aidb_path = os.path.join(temp_dir, f"{locale}.mdb")
+                import_parameters.import_aidb_parameters(
+                    aidb_path=aidb_path, parameters_dir=None, mode="write",
+                    locale="en-CA")
+
+            app.run(
+                {
+                    "output_path": os.path.join(temp_dir, "cbm_defaults.db"),
+                    "schema_path": schema.get_ddl_path(),
+                    "default_locale": "en-CA",
+                    "locales": [
+                        {"id": i_locale, "code": locale}
+                        for i_locale, locale in enumerate(locales)
+                    ],
+                    "archive_index_data": [
+                        {
+                            "locale": locale,
+                            "path": os.path.join(temp_dir, f"{locale}.mdb")
+                        } for locale in locales]
+                })
