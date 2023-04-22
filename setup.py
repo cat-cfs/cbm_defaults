47c04b66a5269eac012600c59ca299285c5fc2c6
Scott <scottm361@gmail.com>
2023-04-21 17:26:10 -0700

    new ver 2.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="cbm_defaults",
-    version="1.2.4",
+    version="2.0.0",
     description="CBM-CFS3 archive-index database to sqlite utility",
     keywords=["cbm-cfs3"],
     long_description=long_description,
